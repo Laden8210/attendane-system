c48bf460a10cb341d30cfd84f0a7ea7312fce9b0
Laden8210 <domingo.laden@gmail.com>
2024-11-12 14:56:59 +0800

    update

--- super-admin/index.php
+++ super-admin/index.php
@@ -4,14 +4,14 @@ require_once '../repository/config.php';
 require_once '../repository/StudentRepository.php';
 require_once '../repository/UserRepository.php';
 require_once '../repository/CourseRepository.php';
-
+if (!isset($_SESSION['officer_id'])) {
+    header('Location: ../student/');
+    exit;
+}
 if (isset($_SESSION['user_type_id'])) {
 
     if (isset($_SESSION['user_type_id']) && $_SESSION['user_type_id'] == 1) {
         header('Location: ../admin/');
-        exit;
-    } elseif (isset($_SESSION['officer_id'])) {
-        header('Location: ../student/');
         exit;
     } else {
         header('Location: ../index.php?view=login'); 
