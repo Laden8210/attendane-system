a4cab8a054c7d91904cb60abecdcc7909c46e696
Laden8210 <domingo.laden@gmail.com>
2024-11-14 08:33:17 +0800

    update

--- admin/controller/student-controller.php
+++ admin/controller/student-controller.php
@@ -2,12 +2,16 @@
 
 require_once '../../repository/StudentRepository.php'; 
 require_once '../../repository/config.php'; 
-
+require_once '../../repository/UserRepository.php'; 
 $studentRepository = new StudentRepository($conn);
+$userRepository = new UserRepository($conn);
+session_start();
+
+$user = $userRepository->readUser($_SESSION['user_id']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if ($_GET['action'] === 'list') {
-        $students = $studentRepository->readAll();
+        $students = $studentRepository->readByCourse($user['course_id']);
         echo json_encode(['status' => 'success', 'students' => $students]);
         exit;
     }
